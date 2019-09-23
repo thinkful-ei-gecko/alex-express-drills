12f273462bd24e13d3755d2e2efe5143354701e5
Alexander Fukui <alexander.fukui@gmail.com>
2019-09-23 14:13:18 -0500

    installed all dependencies (node, nodemon, express, morgan), finished first problem (/sum)

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-test
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/sum', (req, res) => {
+  const a = req.query.a;
+  const b = req.query.b;
+
+  if(!a || !b) {
+    return res.status(400).send('Two integers must be included in query!');
+  }
+
+  const sum = parseInt(a) + parseInt(b)
+  res.send(`
+    The sum of ${a} and ${b} is ${JSON.stringify(sum)}.
+  `);
+});
+
+app.listen(8000, () => {
+  console.log('Express server listening on port 8000');
+});
