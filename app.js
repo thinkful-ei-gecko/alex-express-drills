9818637a4d4aabbd4b22049216696d5809bd7d3c
Alexander Fukui <alexander.fukui@gmail.com>
2019-09-23 15:14:20 -0500

    cipher done.

--- app.js
+++ app.js
@@ -20,6 +20,38 @@ app.get('/sum', (req, res) => {
   `);
 });
 
+app.get('/cipher', (req, res) => {
+  const text = req.query.text.toUpperCase();
+  const shift = parseInt(req.query.shift);
+
+  if (!text || !shift) {
+    return res.status(400).send('Both text and shift are required in query!');
+  }
+
+  const cipher = [];
+
+  //Convert characters to UTF-16 numbers
+  for (var i = 0; i < text.length; i++) {
+    cipher[i] = text.charCodeAt(i);
+  }
+
+  //Shift letter by number in 'shift'; if Z, start from code that is 1 before A.
+  for (var inx = 0; inx < text.length; inx++) {
+    if (cipher[inx] === 90) {
+      cipher[inx] = 64 + shift;
+    } else {
+      cipher[inx] = cipher[inx] + shift;
+    }
+  }
+
+  //Return new numbers to letters.
+  const message = cipher.map(letter => {
+    return String.fromCharCode(letter);
+  }).join('');
+
+  return res.send(`Message: ${message}`);
+});
+
 app.listen(8000, () => {
   console.log('Express server listening on port 8000');
 });
